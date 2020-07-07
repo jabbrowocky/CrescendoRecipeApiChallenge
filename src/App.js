20f348e0fbd8b7e0776f25d6b3e51ac15c6d83ce
Erik White <ewhitedev@gmail.com>
2020-07-07 15:41:18 -0500

    added react-router-dom, and implemented basic navigation schema

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 import ListView from './Components/ListView';
+import Home from './Components/Home';
+import {Switch,Route,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <ListView />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+          </ul>          
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/recipes">
+            <ListView />
+          </Route>
+        </Switch>
+        
     </div>
   );
 }
