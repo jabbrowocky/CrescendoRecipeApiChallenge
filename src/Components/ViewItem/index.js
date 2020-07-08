e461013b81845f1c845eea2ae65d0e64246d1ddf
Erik White <ewhitedev@gmail.com>
2020-07-07 21:13:08 -0500

    view item card displaying data, including image, ingredients and various details

--- src/Components/ViewItem/index.js
+++ src/Components/ViewItem/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import {useParams,Link} from 'react-router-dom';
+import Spinner from "../Spinner";
+import './viewitem.css';
+
 
 export default function ViewItem() {
     const { id } = useParams();
+    const url = `http://localhost:3001/recipes/${id}`;
+    const [recipe, setRecipe] = useState({});
+    const [isLoading, setLoading] = useState(true);
+
+    async function fetchData(url) {
+        const response = await fetch(url);
+        response
+            .json()
+            .then(res => {
+                setRecipe(res);
+                setLoading(false);                
+            }).catch(err => console.log(err));
+    }
+    
+    useEffect(() => {
+        fetchData(url);        
+    }, [url]);
+   
     return(
-        <div>
-            Viewing {id}
+        isLoading 
+        ? 
+        <Spinner />
+        :
+        <div className="recipe-card">
+            <h2>{recipe.title}</h2>
+            <p>{recipe.description}</p>
+            <p>Servings: {recipe.servings} Preptime: {recipe.prepTime} Cooktime: {recipe.cookTime}</p>
+            <div>
+                <h3>Ingredients:</h3>
+                <ul style={{listStyleType:"none"}}>
+                    {recipe.ingredients.map(ingredient => {
+                        return (
+                            <li>
+                                <div>
+                                    <span><em>{`${ingredient.amount} ${ingredient.measurement && ingredient.measurement} `}</em></span>{ingredient.name}
+                                </div> 
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <img className="thumbnail" src={`http://localhost:3001${recipe.images.full}`} alt="thumbnail" />
+            <br />
+            <Link className="foo" to="/recipes">back</Link>
+
         </div>
     )
 }
+
